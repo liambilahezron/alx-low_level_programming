a5a2656463eb495e379c29284e96af81907bbf6c
liambilahezron <liambilahezron1990@gmail.com>
2022-06-02 03:47:53 -0700

    cool

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,25 @@
+/*
+ * file: 4puts.c
+ *Liambila Hezron <@alxschool.com>
+ */
+
+#include <stdio.h>
+
+/**
+ * main-prints "programming is building a multlingual 
+ *              puzzle, followed by new line.
+ *
+ 
+ * Return: Always 0.
+ */
+int main(void)
+{
+        puts("\"programming is like building a mulylingual puzzle");
+
+	return (0);
+}
+
+
+ 
+
+ 
