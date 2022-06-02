03d0e00845b468d7426f98e0fc7be6ebe6338754
liambilahezron <liambilahezron1990@gmail.com>
2022-06-02 03:51:57 -0700

    cool

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -14,7 +14,7 @@
  */
 int main(void)
 {
-        puts("\"programming is like building a mulylingual puzzle");
+        puts("\"programming is like building a multilingual puzzle");
 
 	return (0);
 }
