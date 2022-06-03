43853af12cf9dbab42c73a296d2227166b56404d
liambilahezron <liambilahezron1990@gmail.com>
2022-06-02 23:26:33 -0700

    cool

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -3,14 +3,22 @@
 /**
  * main - Entry point
  *
- * Description: 'and that piece of art is useful" - Dora korpa, 2015-10-19'
- *
  * Return: Always 0 (success)
  */
 
 int main(void)
 {
-	write(1, "and that piece of art is useful\" -Dora Korpar' 2015-10-19\n", 59);
-	return (1);
+	char acharactor;
+	int ainteger;
+	long along;
+	long long alonglong;
+	float afloat;
+
+	printf("Size of a char: %lu byte(s)\n", sizeof(acharacter));
+	printf("Size of an int: %lu byte(s)\n", sizeof(ainteger));
+	printf("Size of a long int: %lu byte(s)\n", sizeof(along));
+	printf("Size of a long long int: %lu byte(s)\n", sizeof(alonglong));
+	printf("Size of a float: %lu byte(s)\n", sizeof(afloat));
+
 }
 
