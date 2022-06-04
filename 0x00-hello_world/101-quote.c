8eebdda1e318c4cb7e60d4aaa78e140f17ba4ef8
liambilahezron <liambilahezron1990@gmail.com>
2022-06-04 06:34:31 -0700

    cool

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -2,16 +2,14 @@
 #include <stdio.h>
 
 /**
- * main - Entry point
+ * main - prints exactly and that piece of art is useful
  *
- * Description: 'and that piece of art is useful" - Dora Korpa, 2015-10-19'
- *
- * Return: Always 0 (success)
+ * Return: Always 0
  */
-
 int main(void)
 {
-	write(1, "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", 59);
+	write(2, "and that piece of art is useful\" - Dora Korpar,
+			2015-10-19\n", 59);
+
 	return (1);
 }
-
