0f85d441adb923cadf68b498fc8c16c659435812
liambilahezron <liambilahezron1990@gmail.com>
2022-06-07 05:30:08 -0700

    putchar

--- 0x02-functions_nested_loops/1-alphabet.c
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -1,11 +1,9 @@
 #include "main.h"
 
 /**
- * main - check the code
- *
- * Return: Always 0
+ * main - prints the alphabet in lowercase, followed by a new line
  */
-int main(void)
+void print_alphabet(void)
 {
 	char letter;
 
@@ -13,6 +11,4 @@ int main(void)
 		_putchar(letter);
 
 	_putchar('\n');
-
-	return (0);
 }
