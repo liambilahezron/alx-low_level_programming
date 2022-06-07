d3403c64bc37e7051c2196a4d25f48820605d489
liambilahezron <liambilahezron1990@gmail.com>
2022-06-07 15:34:02 -0700

    putchar

--- 0x02-functions_nested_loops/100-times_table.c
+++ 0x02-functions_nested_loops/100-times_table.c
@@ -15,6 +15,7 @@ void print_times_table(int n)
 		{
 			_putchar('0');
 
+
 			for (mult = 1; mult <= n; mult++)
 			{
 				_putchar(',');
@@ -42,4 +43,3 @@ void print_times_table(int n)
 		}
 	}
 }
-
