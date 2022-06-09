aa75ff676d631498daad5ee9e0ebd5cc2d5e3194
liambilahezron <liambilahezron1990@gmail.com>
2022-06-09 03:45:27 -0700

    hood

--- 0x04-more_functions_nested_loops/5-more_numbers.c
+++ 0x04-more_functions_nested_loops/5-more_numbers.c
@@ -9,7 +9,7 @@ void more_numbers(void)
 
 	for (count = 0; count <= 9; count++)
 	{
-		for (num = 0; num <= 14; count++)
+		for (num = 0; num <= 14; num++)
 		{
 			if (num > 9)
 				_putchar((num / 10) + '0');
