611902dfcabea857ec0f66bb4e4d73478965baa0
liambilahezron <liambilahezron1990@gmail.com>
2022-06-22 05:55:43 -0700

    recursion

--- 0x08-recursion/1-print_rev_recursion.c
+++ 0x08-recursion/1-print_rev_recursion.c
@@ -1,9 +1,8 @@
 #include "main.h"
 
 /**
- * _print_rev_recursion - reverse
- * @s: pointer to string params
- *
+ * _print_rev_recursion - prints a string in reverse
+ * @s: the string to be printed
  */
 void _print_rev_recursion(char *s)
 {
