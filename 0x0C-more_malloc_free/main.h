65a4119128b5699250859917d7066a1714596cd6
liambilahezron <liambilahezron1990@gmail.com>
2022-06-30 16:26:59 +0300

    function that concatenates two strings

--- 0x0C-more_malloc_free/main.h
+++ 0x0C-more_malloc_free/main.h
@@ -1,14 +1,7 @@
-#include <stdlib.h>
-#include <stdio.h>
-
-int _putchar(char c);
+#ifndef _main_h
+#define _main_h
 void *malloc_checked(unsigned int b);
 char *string_nconcat(char *s1, char *s2, unsigned int n);
 void *_calloc(unsigned int nmemb, unsigned int size);
 int *array_range(int min, int max);
-void errors(void);
-int _strlen(char *s);
-int is_digit(char *s);
-void *_realloc(void *ptr, unsigned int old_size, unsigned int n);
-int main(int argc, char *argv[])
-
+#endif
