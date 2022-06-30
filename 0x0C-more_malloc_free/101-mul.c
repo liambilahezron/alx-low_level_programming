f2689b4cfc9afb07fb73f78df2aee390a0984252
liambilahezron <liambilahezron1990@gmail.com>
2022-06-30 16:54:08 +0300

    multiplies two positive numbers

--- 0x0C-more_malloc_free/101-mul.c
+++ 0x0C-more_malloc_free/101-mul.c
@@ -1,29 +1,26 @@
-#include "main.h"
-
+#include <stdio.h>
+#include <stdlib.h>
+#define UNUSED(x)(void)(x)
 /**
- * main - multiplies two positive numbers
- * @argc: n arguments
- * @argv: args
- * Return: int
+ * main - multiplies two numbers
+ * @argc: int
+ * @argv: array of strings
+ * Return: sum
  */
 int main(int argc, char *argv[])
 {
-unsigned long mul;
-int i, j;
-	if (argc != 3)
-	{ printf("Error\n");
-	exit(98); }
-	for (i = 1; i < argc; i++)
-	{
-		for (j = 0; argv[i][j] != '\0'; j++)
-		{
-			if (argv[i][j] > 57 || argv[i][j] < 48)
-			{  printf("Error\n");
-			exit(98); }
-		}
+	int i;
+	int j;
 
+	if (argc < 3)
+	{
+		printf("%s\n", "Error");
+		return (1);
 	}
-	mul = atol(argv[1]) *  atol(argv[2]);
-	printf("%lu\n", mul);
-return (0);
+
+	i = atoi(argv[1]);
+	j = atoi(argv[2]);
+
+	printf("%d\n", i * j);
+	return (0);
 }
