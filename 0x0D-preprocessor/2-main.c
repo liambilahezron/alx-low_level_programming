db867ec7a53e823d37d24a0763f71d9dc1e27a87
liambilahezron <liambilahezron1990@gmail.com>
2022-07-04 12:25:50 +0300

    prints the files that was compile from

--- 0x0D-preprocessor/2-main.c
+++ 0x0D-preprocessor/2-main.c
@@ -1,12 +1,14 @@
-#include <stdio.h.
+#include <stdio.h>
 
 /**
-* main - check code for alx students
-*
-* Return: Always 0
-*/
+ * main - Prints the name of the file of the program
+ *        was compiled from, followed by a new line.
+ *
+ * Return: Always 0.
+ */
 int main(void)
 {
-printf("%s\n", __file__);
-return (0);
+	printf("%s\n", __FILE__);
+
+	return (0);
 }
