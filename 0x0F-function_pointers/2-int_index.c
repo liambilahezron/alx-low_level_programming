83144628ddfbe953275d7805499c23c378c97099
liambilahezron <liambilahezron1990@gmail.com>
2022-07-06 12:07:44 +0300

    functions that searches index

--- 0x0F-function_pointers/2-int_index.c
+++ 0x0F-function_pointers/2-int_index.c
@@ -1,30 +1,30 @@
-#include "funtion_pointers.h"
+#include "function_pointers.h"
 
 /**
-* int_intex - ...
-* @array: ...
-* @size: ...
-* @cmp: ...
-*
-*Return: ...
-*/
+  * int_index - ...
+  * @array: ...
+  * @size: ...
+  * @cmp: ...
+  *
+  * Return: ...
+  */
 int int_index(int *array, int size, int (*cmp)(int))
 {
 	int i = 0;
-	
-	if (size > o)
+
+	if (size > 0)
 	{
 		if (array != NULL && cmp != NULL)
 		{
 			while (i < size)
 			{
-				if (cmp(array[i])
+				if (cmp(array[i]))
 					return (i);
-					
-					i++;
+
+				i++;
 			}
 		}
 	}
-	
+
 	return (-1);
 }
